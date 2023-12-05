bc9559057e2e1d712958d9cca1cb5c122edf94c8
shiory602 <suzushioufo@gmail.com>
2023-12-05 10:21:37 +0400

    fix scroll top

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// slider の設定
 function slideSwitch() {
   var $active = $('#slideshow img.active');
 
@@ -17,4 +18,41 @@ function slideSwitch() {
 
 $(function() {
   setInterval( "slideSwitch()", 5000 );
+});
+
+
+
+
+// スクロールアップの設定//////////////////
+$(function() {
+  // 変数にクラスを入れる
+  var btn = $('.pageTop');
+  
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).on('load scroll', function(){
+    if($(this).scrollTop() > 100) {
+      btn.addClass('active');
+    }else{
+      btn.removeClass('active');
+    }
+  });
+  
+  //フッターの手前でボタンを止める
+  $(window).on('load scroll', function(){
+    var height = $(document).height(), //ドキュメントの高さ 
+        position = window.innerHeight + $(window).scrollTop(), //ページトップから現在地までの高さ
+        footer = $('footer-inr').height(); //フッターの高さ
+    if ( height - position  < footer ){ 
+      btn.addClass('absolute');
+    } else { 
+      btn.removeClass('absolute');
+    }
+  });
+
+  //スクロールしてトップへ戻る
+  btn.on('click',function () {
+    $('body,html').animate({
+      scrollTop: 0
+    });
+  });
 });
