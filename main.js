5b0472cfc0a19cc4563ea8231fa0cfdeee1a5dea
shiory602 <suzushioufo@gmail.com>
2023-12-17 14:36:03 +0400

    mobile menu

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
-// slider の設定
+////////////////////
+/* ハンバーガーmenu */
+////////////////////
+// 追従ナビ・スマホでハンバーガーメニューになる
+(function($) {
+  $(function() {
+      var $header = $('#head_wrap');
+      // Nav Fixed
+      $(window).scroll(function() {
+          if ($(window).scrollTop() > 350) {
+              $header.addClass('fixed');
+          } else {
+              $header.removeClass('fixed');
+          }
+      });
+      // Nav Toggle Button
+      $('#nav-toggle, #global-nav ul li a').click(function(){
+          $header.toggleClass('open');
+      });
+  });
+})(jQuery);
+
+// ゆっくりスクロールする
+$(function(){
+  $('a[href^="#"]').click(function() {
+    var adjust = 0;
+    var speed = 1200;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
+
+//////////////////
+/* slider の設定 */
+//////////////////
+
 function slideSwitch() {
   var $active = $('#slideshow img.active');
 
@@ -23,7 +61,9 @@ $(function() {
 
 
 
-// スクロールアップの設定//////////////////
+////////////////////////
+/* スクロールアップの設定 */
+////////////////////////
 $(function() {
   // 変数にクラスを入れる
   var btn = $('.pageTop');
