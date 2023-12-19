9da5d59d851c6c31c128c93ff9b6ab9798916f06
shiory602 <suzushioufo@gmail.com>
2023-12-19 15:20:35 +0400

    fix css

--- main.js
+++ main.js
@@ -21,17 +21,17 @@
 })(jQuery);
 
 // ゆっくりスクロールする
-$(function(){
-  $('a[href^="#"]').click(function() {
-    var adjust = 0;
-    var speed = 1200;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top + adjust;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    return false;
-  });
-});
+// $(function(){
+//   $('a[href^="#"]').click(function() {
+//     var adjust = 0;
+//     var speed = 1200;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? 'html' : href);
+//     var position = target.offset().top + adjust;
+//     $('body,html').animate({scrollTop:position}, speed, 'swing');
+//     return false;
+//   });
+// });
 
 //////////////////
 /* slider の設定 */
